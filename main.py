ebc3116d1f56cce63121c3f8521ae1b7bf9e7ea3
Arvin <arvin.yuwono@binus.ac.id>
2023-06-18 12:48:29 +0700

    set threshold manually

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def run_simulation():
 
     # Initialize PyBullet
     p.connect(p.GUI)
-    #Set the gravity to point downwards
     p.setGravity(0, 0, -9.81)
 
     # Create the plane
@@ -16,67 +15,83 @@ def run_simulation():
     plane_body_id = p.createMultiBody(0, plane_id, plane_visual_id)
 
     # Create the phone
-    phone_weight = float(weight_entry.get())/1000 #convert gr to kg
+    phone_weight = float(weight_entry.get()) / 1000  # convert grams to kilograms
 
     base_width = float(width_entry.get())
     base_depth = float(depth_entry.get())
     base_height = float(height_entry.get())
     drop_height = float(drop_height_entry.get())
-    
+
     phone_id = p.createCollisionShape(p.GEOM_BOX, halfExtents=[base_width, base_depth, base_height])
     phone_visual_id = p.createVisualShape(p.GEOM_BOX, halfExtents=[base_width, base_depth, base_height], rgbaColor=[1, 0, 0, 1])
     phone_body_id = p.createMultiBody(phone_weight, phone_id, phone_visual_id)
-    p.resetBasePositionAndOrientation(phone_body_id, [0, 0, drop_height],
-                                      [float(orientation_entry[0].get()), float(orientation_entry[1].get()), float(orientation_entry[2].get()), 1])
+    p.resetBasePositionAndOrientation(
+        phone_body_id, [0, 0, drop_height], [float(orientation_entry[0].get()), float(orientation_entry[1].get()), float(orientation_entry[2].get()), 1]
+    )
 
-    max_impact_energy = 0
-    phone_rest_threshold = 0.001 
+
+    phone_rest_threshold = 0.001
+
     # Damage Thresholds
-    threshold_minor_percentage = 10
-    threshold_moderate_percentage = 30
-    threshold_severe_percentage = 70
+   
 
+    
     impact_energies = []
-    highest_damage_level = None
+    highest_damage_level = 0
+
     # Run the simulation
     while True:
         p.stepSimulation()
-        time.sleep(1/240)  # Delay to control the simulation speed
+        time.sleep(1 / 240)  # Delay to control the simulation speed
 
-        #Calculating the impact energy (one of the ways for the damage)
-        phone_mass = p.getDynamicsInfo(phone_body_id,-1)[0]
+        # Calculating the impact energy
+        phone_mass = p.getDynamicsInfo(phone_body_id, -1)[0]
         phone_velocity, phone_angular_velocity = p.getBaseVelocity(phone_body_id)
-        phone_velocity_magnitude = math.pow(math.pow(phone_velocity[0], 2) + math.pow(phone_velocity[1], 2) + math.pow(phone_velocity[2], 2), 0.5)
-        impact_energy = 0.5 * phone_mass * (phone_velocity_magnitude ** 2)
+        phone_velocity_magnitude = math.sqrt(phone_velocity[0] ** 2 + phone_velocity[1] ** 2 + phone_velocity[2] ** 2)
+        initial_potential_energy = phone_mass * 9.81 * drop_height
+        final_potential_energy = phone_mass * 9.81 * 0
+        impact_energy = initial_potential_energy - final_potential_energy
 
         impact_energies.append(impact_energy)
-        print(f"Impact energy: {impact_energy}")
-        if impact_energy>max_impact_energy:
-            max_impact_energy   = impact_energy
+        
+        
+        
         if phone_velocity_magnitude < phone_rest_threshold and all(v < 0.01 for v in phone_angular_velocity):
-
             break
 
-    print(f"Maximum impact energy: {max_impact_energy}")
     
+    max_impact_energy = 0
+    total_energy = 0
     # Calculate the adjusted thresholds based on the maximum impact energy
-    threshold_minor = threshold_minor_percentage * max_impact_energy / 100
-    threshold_moderate = threshold_moderate_percentage * max_impact_energy / 100
-    threshold_severe = threshold_severe_percentage * max_impact_energy / 100
+    for impact_energy in impact_energies:
+        total_energy += impact_energy
+        max_impact_energy = max(max_impact_energy, impact_energy)
+
+    threshold_minor = 0.5
+    threshold_moderate = 1.0
+    threshold_severe = 2.0
 
 
     # Calculating the damage
     for impact_energy in impact_energies:
-        if (impact_energy < threshold_minor):
-            highest_damage_level = "No Significant damage."
-        elif (impact_energy < threshold_moderate):
-           highest_damage_level="Minor damage. Functional with cosmetic damage."
-        elif (impact_energy < threshold_severe):
-            highest_damage_level="Moderate damage. Noticeable structural damage."
+        if impact_energy >= threshold_severe:
+            highest_damage_level = 4
+            print()
+        elif impact_energy >= threshold_moderate:
+            highest_damage_level = max(highest_damage_level, 3)
+        elif impact_energy >= threshold_minor:
+            highest_damage_level = max(highest_damage_level, 2)
         else:
-            highest_damage_level="Extensive damage. Barely functional or non-functional"
+            highest_damage_level = max(highest_damage_level, 1)
 
-    print(highest_damage_level)
+    if highest_damage_level == 4:
+        print("Extensive damage. Barely functional or non-functional")
+    elif highest_damage_level == 3:
+        print("Moderate damage. Noticeable structural damage.")
+    elif highest_damage_level == 2:
+        print("Minor damage. Functional with cosmetic damage.")
+    else:
+        print("No significant damage.")
 
     # Keep the window open until explicitly closed
     while True:
@@ -84,19 +99,18 @@ def run_simulation():
         time.sleep(0.01)
 
 def fill_dimension_values(weight, width, depth, height):
-    weight_entry.delete(0, weight_entry.__sizeof__())
-    width_entry.delete(0, width_entry.__sizeof__())
-    depth_entry.delete(0, depth_entry.__sizeof__())
-    height_entry.delete(0, height_entry.__sizeof__())
+    weight_entry.delete(0, tk.END)
+    width_entry.delete(0, tk.END)
+    depth_entry.delete(0, tk.END)
+    height_entry.delete(0, tk.END)
     weight_entry.insert(0, weight)
     width_entry.insert(0, width)
     depth_entry.insert(0, depth)
     height_entry.insert(0, height)
 
-window = tk.Tk() #Create a UI
+window = tk.Tk()  # Create a UI
 label = tk.Label(window, text="Adjust the value according to your needs!")
 label.grid(row=0, column=0, columnspan=2)
-
 
 weight_label = tk.Label(window, text="Weight (gr):")
 weight_label.grid(row=1, column=0)
@@ -126,7 +140,7 @@ drop_height_label = tk.Label(window, text="Drop Height (m):")
 drop_height_label.grid(row=5, column=0)
 drop_height_entry = tk.Entry(window)
 drop_height_entry.grid(row=5, column=1)
-drop_height_entry.insert(0, 5)
+drop_height_entry.insert(0, 0.1)
 
 orientation_entry_label = tk.Label(window, text="Orientation° (x, y, z): ")
 orientation_entry_label.grid(row=6, column=0)
@@ -138,16 +152,16 @@ orientation_entry[1].insert(0, 0)
 orientation_entry[2].grid(row=6, column=3)  # z
 orientation_entry[2].insert(0, 0)
 
-simulation_button = tk.Button(window, text="Begin Simulation",command=lambda:run_simulation())
+simulation_button = tk.Button(window, text="Begin Simulation", command=lambda: run_simulation())
 simulation_button.grid(row=7, column=0, columnspan=4)
 
 presets_label = tk.Label(window, text="Presets")
 presets_label.grid(row=0, column=2, columnspan=2)
 
-preset1_button = tk.Button(window, text="Phone", command=lambda:fill_dimension_values(175, 0.075, 0.008, 0.16))
+preset1_button = tk.Button(window, text="Phone", command=lambda: fill_dimension_values(175, 0.075, 0.008, 0.16))
 preset1_button.grid(row=2, column=2, columnspan=2)
 
-preset2_button = tk.Button(window, text="Tablet", command=lambda:fill_dimension_values(400, 0.160, 0.007, 0.24))
+preset2_button = tk.Button(window, text="Tablet", command=lambda: fill_dimension_values(400, 0.16, 0.007, 0.24))
 preset2_button.grid(row=4, column=2, columnspan=2)
 
-window.mainloop() #Launch the UI in an endless loop
+window.mainloop()  # Launch the UI in an endless loop
